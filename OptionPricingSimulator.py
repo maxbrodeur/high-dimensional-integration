4307dc8556070518129b73bfe9c450ce9225845b
Max Brodeur <maxbrod21@gmail.com>
2024-01-13 14:02:14 +0100

    Changed everything to column vectors to match math

--- OptionPricingSimulator.py
+++ OptionPricingSimulator.py
@@ -23,22 +23,32 @@ class OptionPricingSimulator:
         self.dt = self.T/m
         self.t = np.arange(1, self.m+1)*self.dt
 
-    # stock price analytic solution
+    """
+    Stock price analytic solution
+    params:
+        w: Brownian motion path (m,N) (column vector for each N)
+    return: S(t) (m,N)
+    """
     def S(self, w: np.ndarray) -> np.ndarray:
         time_dependent_coeff = self.S0*np.exp((self.r - self.sigma**2/2)*self.t) # shape (m,1)
-        brownian_motion_coeff = np.exp(self.sigma*np.einsum('ij,j->ij', w, self.t)) # shape (N,m)
-        S = np.einsum('ij,j->ij', brownian_motion_coeff, time_dependent_coeff) # shape (N,m)
+        brownian_motion_coeff = np.exp(self.sigma*np.einsum('ij,i->ij', w, self.t)) # shape (m,N)
+        S = np.einsum('ij,i->ij', brownian_motion_coeff, time_dependent_coeff) # shape (m,N)
         return S
 
-    # jump part of payoff function (average of S(t) over time)
+    """
+    Jump part of payoff function
+    params:
+        w: Brownian motion path (m,N) (column vector for each N)
+    return: phi(w) (m,N)
+    """
     def phi(self, w: np.ndarray) -> np.ndarray:
-        assert self.t.shape[0] == w.shape[1], f"should be as many time steps as values in Brownian motion path, got {self.t.shape[0]} time steps and {w.shape[1]} values in Brownian motion path"
+        assert self.t.shape[0] == w.shape[0], f"should be as many time steps as values in Brownian motion path, got {self.t.shape[0]} time steps and {w.shape[0]} values in Brownian motion path"
         # payoff is average of S(t) over time over strike price 
-        return np.sum(self.S(w), axis=1)/self.m - self.K
+        return np.sum(self.S(w), axis=0)/self.m - self.K
     
     # Asian call option (Psi_1)
     def Asian(self, w: np.ndarray) -> float:
-        mask = (self.phi(w)>0)
+        mask = self.phi(w)>0
         return self.phi(w)*mask
     
     # binary digital Asian option (Psi_2)
@@ -58,48 +68,59 @@ class OptionPricingSimulator:
     def build_eta(self, N: int) -> np.ndarray:
         n_vars = int(2**N)
         eta = np.zeros((self.m, n_vars))
-        for t in range(self.m):
+        for m, t in enumerate(self.t):
             for k in range(n_vars):
-                ti = (t+1)*self.dt
                 if k == 0:
-                    eta[t,k] = ti
+                    eta[m,k] = t
                 else:
                     # k = 2^(n-1) + i with i = 1,...,2^(n-1)
                     n = np.floor(np.log2(k)) + 1
                     i = (k+1) - 2**(n-1)
-                    eta[t,k] = self.eta(n,i,ti)
+                    eta[m,k] = self.eta(n,i,t)
         return eta
 
-    def eta(self, n: int, i: int, ti: float) -> float:
+    def eta(self, n: int, i: int, t: float) -> float:
         left = (2*i-2)/(2**n)
         middle = (2*i-1)/(2**n)
         right = (2*i)/(2**n)
-        if ti < left or ti > right:
+        if t < left or t > right:
             return 0
-        elif ti < middle:
-            return 2**((n-1)/2)*(ti-(2*i-2)/(2**n))
+        elif t < middle:
+            return 2**((n-1)/2)*(t-(2*i-2)/(2**n))
         else:
-            return 2**((n-1)/2)*((2*i)/(2**n)-ti) # TODO: what to do with ti=middle
+            return 2**((n-1)/2)*((2*i)/(2**n)-t) # TODO: what to do with ti=middle
         
-    # generate uniform vectors
+    """
+    Generate uniform column vectors
+    params:
+        N: number of vectors
+    return: y (m,N) (column vector for each N)
+    """
     def generate_uniform_vectors(self, N:int) -> np.ndarray:
-        return st.uniform.rvs(size=(N, self.m))
-
-    # multivariate standard normal CDF inverse
+        return np.random.uniform(size=(self.m,N))
+    
+    """
+    CDF inverse of standard normal distribution
+    params:
+        y: uniform vectors
+    return: CDF_inverse(y)
+    """
     def CDF_inverse(self, y: np.ndarray) -> np.ndarray:
         return st.norm.ppf(y)
     
     """ Monte Carlo simluation
         psi: payoff function (Asian or Asian_binary)
-        y: uniform vectors 
+        y: uniform column vectors (m,N)
         matrix: transformation matrix (Cholesky or Levy-Ciesielski)
     """
     def MC(self, psi: Callable, y: np.ndarray, matrix: np.ndarray) -> float:
+        N = y.shape[1]
         interest_coeff = np.exp(-self.r*self.T)
-        Psi_vars = psi(self.CDF_inverse(y)@matrix)
+        Psi_vars = psi(matrix@self.CDF_inverse(y)) # transpose since its eta @ column vector
+        assert len(Psi_vars) == N, f"Psi(matrix@CDF_inverse(y)) should be a vector of length {N}, got shape {Psi_vars.shape}"
         MC_mean = np.mean(Psi_vars)
         var = np.var(Psi_vars)
-        mse = var/y.shape[0]
+        mse = var/N
         return interest_coeff * MC_mean, mse
     
     """ Crude Monte Carlo simulation
@@ -109,10 +130,13 @@ class OptionPricingSimulator:
     def crude_MC(self, psi: Callable, N: int, transformation: str = "Cholesky") -> float:
         if transformation == "Cholesky":
             A = np.linalg.cholesky(self.build_C())
-            return self.MC(psi, self.generate_uniform_vectors(N), A)
+            y = self.generate_uniform_vectors(N)
+            return self.MC(psi, y, A)
         elif transformation == "Levy-Ciesielski":
-            eta = self.build_eta(np.log2(self.m))
-            return self.MC(psi, self.generate_uniform_vectors(N), eta)
+            LC_N = int(np.log2(self.m))
+            eta = self.build_eta(LC_N)
+            y = self.generate_uniform_vectors(N)
+            return self.MC(psi, y, eta)
         else:
             raise NotImplementedError(f"transformation {transformation} not implemented")
 
@@ -122,15 +146,20 @@ class OptionPricingSimulator:
         K: number of RQMC simulations
     """
     def randomized_QMC(self, psi: Callable, N: int, K: int, transformation: str = "Cholesky") -> float:
-        P = sn.generate_points(N,self.m)
-        U = self.generate_uniform_vectors(K)
+        P = sn.generate_points(self.m, N) # shape (m,N)
+        U = self.generate_uniform_vectors(K).T # shape (K,m)
         Vi_list = np.zeros(K)
         for i in range(K):
-            shifted_P = (P + U[i]) % 1
+            shift = U[i].reshape(-1,1) # reshape to column vector
+            shifted_P = (P + shift) % 1
+            assert shifted_P.shape == P.shape, f"shifted_P shape {shifted_P.shape} should be equal to P shape {P.shape}"
             if transformation == "Cholesky":
-                Vi_list[i], var = self.Cholesky_MC(psi, shifted_P)
+                A = np.linalg.cholesky(self.build_C())
+                Vi_list[i], var = self.MC(psi, shifted_P, A)
             elif transformation == "Levy-Ciesielski":
-                Vi_list[i], var = self.Levy_Ciesielski_MC(psi, shifted_P)
+                LC_N = int(np.log2(self.m))
+                eta = self.build_eta(LC_N)
+                Vi_list[i], var = self.MC(psi, shifted_P, eta)
             else:
                 raise NotImplementedError(f"transformation {transformation} not implemented")
         mse = np.var(Vi_list)/K # TODO: should we consider the interest coefficient here?
